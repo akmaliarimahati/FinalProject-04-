0e7493b43f1767db2ccdb827ed90b70d32ad8292
Nindyaull <pindangoreng123@gmail.com>
2025-10-03 21:56:40 +0700

    modifikasi user 2

--- Test.java
+++ Test.java
@@ -2,5 +2,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hi, ini baris kode pertama");
         System.out.println("User 2 menambahkan baris baru");
+
+        System.out.println("Hai, ini baris kode pertama yang dimodifikasi user 2");
+        System.out.println("User 2 menambah baris baru");
     }
 }
